3244adcae73a361e3bc50fa23bd22e6dc9ecd652
elena.kolyshkina <elena.kolyshkina@gmail.com>
2013-08-15 00:21:50 +0300

    change constant value

--- magicbox-processor/src/main/java/com/magicbox/processor/Constants.java
+++ magicbox-processor/src/main/java/com/magicbox/processor/Constants.java
@@ -1,12 +1,12 @@
 package com.magicbox.processor;
 
 public interface Constants {
-	String DEFAULT_CONTEXT_NAME = "magicboxmodule";
-	String DEFAULT_CONTEXT_CLASS_NAME = "com.magicbox.gen.MagicBoxModule";
+	String DEFAULT_CONTEXT_NAME = "magicboxcontext";
+	String DEFAULT_CONTEXT_CLASS_NAME = "com.magicbox.gen.MagicBoxContext";
 	String DEFAULT_PACKAGE = "com.magicbox.gen";
 	String SUFFIX = "BeanDef";
 
-	final String PROPS_NAME = ".magicbox";
-	final String PROPS_PATH = "META-INF";
+	String PROPS_NAME = ".magicbox";
+	String PROPS_PATH = "META-INF";
 	String SEPARATOR = ":";
 }
